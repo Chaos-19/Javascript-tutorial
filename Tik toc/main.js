09e5f91fe8f31ed11b0843666c337e01641f29f5
kalgetachew375@gmail.com <kalgetachew375@gmail.com>
2023-06-12 17:02:53 +0300

    optimising

--- Tik toc/main.js
+++ Tik toc/main.js
@@ -1,6 +1,6 @@
 var boxs = document.querySelectorAll('.wrapper');
-
-/*boxs.forEach(function(val, index) {
+/*
+boxs.forEach(function(val, index) {
 
 
   let clicked = false;
@@ -29,10 +29,10 @@ var boxs = document.querySelectorAll('.wrapper');
     }
     const boxValue = container.map((e) => { return e.textContent; });
 
-    const cellValue = create3x3Array(boxValue);
+    const cellValue =[] //create3x3Array(boxValue);
 
 
-    const winner = checkWinningLine(cellValue);
+    const winner = []//checkWinningLine(cellValue);
 
     if (winner == 'X') {
       console.log('X WIN 🏆 ')
@@ -47,23 +47,65 @@ var boxs = document.querySelectorAll('.wrapper');
 })
 */
 
+
+
 var bordCell = document.getElementsByClassName('item');
 for (var i = 0; i < bordCell.length; i++) {
   bordCell[i].addEventListener('click', clickHandler);
 }
 
+var currentPlayer = 'X';
+var cellValue = [];
+
+
 function clickHandler(event) {
   var targetContent = event.target;
-  
-  if(!event.target.textContent)
-  event.target.textContent =currentPlayer? 'X' : 'O';
-  
-  console.log(targetContent.textContent)
-  currentPlayer = true;
+  var index = Array.from(bordCell).indexOf(event.target);
+
+
+  cellValue[index] = currentPlayer;
+
+  if (!event.target.textContent)
+    event.target.textContent = currentPlayer == 'X' ? 'X' : 'O';
+
+
+
+  if (checkWinner()) {
+    onWinner(currentPlayer)
+  }
+
+
+  if (currentPlayer == 'X') {
+    currentPlayer = 'O';
+  } else {
+    currentPlayer = 'X';
+  }
+
 }
 
 
 
-document.querySelector('.btn').addEventListener('click', e => {
-  document.querySelector('.banner').style.transform = 'scale(1)';
-})
+document.querySelector('.btn').addEventListener('click', restButn)
+
+function onWinner(text) {
+  let bann = document.querySelector('.banner');
+  bann.style.transform = 'scale(1)';
+  bann.textContent = text;
+}
+
+function checkWinner() {
+  var winningCompination = [
+   [0, 1, 2], [3, 4, 5], [6, 7, 8],
+   [0, 3, 6], [1, 4, 7], [2, 3, 8],
+   [0, 4, 8], [2, 4, 6]
+
+    ];
+
+  for (let combination of winningCompination) {
+    const [a, b, c] = combination;
+    if (cellValue[a] !== '' && cellValue[a] === cellValue[b] && cellValue[a] === cellValue[c]) {
+      return true;
+    }
+  }
+  return false;
+}
